46269300933b59950c2a019f62bc4248f71357a5
Daniel Kubec <dkubec@securityscorecard.io>
2019-01-21 02:40:08 +0100

    Optimize clang warning output.

--- .vim/bin/cmake_clang_complete.sh
+++ .vim/bin/cmake_clang_complete.sh
@@ -6,3 +6,5 @@ cmake .. -DCMAKE_CXX_COMPILER=$HOME/.vim/bin/wrap_cxx_clang.sh \
 make "$@"
 echo -n "" > $build_dir/../.clang_complete
 find . -type f -name .clang_complete -exec cat {} >> $build_dir/../.clang_complete \;
+echo "-Wno-unused-parameter" >> $build_dir/../.clang_complete
+echo "-Wno-unused-function" >> $build_dir/../.clang_complete
