4c34ad1641149fd2f6793d1d792858e4b6afdcf5
Daniel Kubec <dkubec@securityscorecard.io>
2019-01-23 23:59:03 +0100

    Remove pedantic flag.

--- .vim/bin/cmake_clang_complete.sh
+++ .vim/bin/cmake_clang_complete.sh
@@ -5,6 +5,8 @@ cmake .. -DCMAKE_CXX_COMPILER=$HOME/.vim/bin/wrap_cxx_clang.sh \
          -DCMAKE_C_COMPILER=$HOME/.vim/bin/wrap_cc_clang.sh
 make "$@"
 echo -n "" > $build_dir/../.clang_complete
-find . -type f -name .clang_complete -exec cat {} >> $build_dir/../.clang_complete \;
+find . -type f -name .clang_complete -exec cat {} >> $build_dir/../.clang_complete.1 \;
+sed -n '/pedantic/!p' $build_dir/../.clang_complete.1 > $build_dir/../.clang_complete 
+rm $build_dir/../.clang_complete.1
 echo "-Wno-unused-parameter" >> $build_dir/../.clang_complete
 echo "-Wno-unused-function" >> $build_dir/../.clang_complete
